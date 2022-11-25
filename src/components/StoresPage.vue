b2aeaf587027f410aa698f60fa686efc2d2aa0f4
Puri12 <tkdqja9812@gmail.com>
2022-11-25 11:16:05 +0900

    feat : StoreList 추가

--- src/components/StoresPage.vue
+++ src/components/StoresPage.vue
@@ -1,15 +1,95 @@
 <template>
   <div class="StoresPage">
-    <h1>Stores</h1>
-    <p>
-      This is the stores page.
-    </p>
+    <h1>Stores List</h1>
   </div>
+  <v-table>
+    <thead>
+    <tr>
+      <th class="text-left">
+        id
+      </th>
+      <th class="text-left">
+        Name
+      </th>
+      <th class="text-left">
+        address
+      </th>
+      <th class="text-left">
+        category
+      </th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="store in stores" :key="store.storeId">
+      <td class="text-left">
+        {{ store.storeId }}
+      </td>
+      <td class="text-left">
+        {{ store.name }}
+      </td>
+      <td class="text-left">
+        {{ store.address }}
+      </td>
+      <td class="text-left">
+        {{ store.category }}
+      </td>
+    </tr>
+    </tbody>
+  </v-table>
+  <v-pagination
+      v-model="page"
+      :length="length"
+      @click="getStoresByPage()"
+      @next="nextPage"
+      @previous="prevPage"
+  ></v-pagination>
 </template>
 
 <script>
+import axios from "axios";
+const API_URL = import.meta.env.VITE_API_URL;
+
 export default {
-  name: "StoresPage"
+  name: "StoresPage",
+  data() {
+    return {
+      stores: [],
+      page: 1,
+      length: 0
+    };
+  },
+  methods: {
+    getStoresByPage() {
+      axios
+        .get(API_URL + "/api/stores?page=" + (this.page - 1))
+        .then(response => {
+          console.log(response.data);
+          this.stores = response.data.data.content;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    nextPage() {
+      this.getStoresByPage(this.page);
+    },
+    prevPage() {
+      this.getStoresByPage(this.page);
+    },
+
+  },
+  mounted() {
+    axios
+      .get( API_URL+ "/api/stores?page=" + --this.page)
+      .then(response => {
+        console.log(response.data);
+        this.stores = response.data.data.content;
+        this.length = response.data.data.totalPages;
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  },
 }
 </script>
 
