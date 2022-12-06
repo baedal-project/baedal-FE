f030c1258febc4750072c9b4ac786d639f53cf76
Puri12 <tkdqja9812@gmail.com>
2022-12-07 00:56:00 +0900

    feat : Main 작업
    - 오더기능 추가
    - 상세조회 추가
    - 오더 리스트 추가

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="HomePage">
-    <h1>Home</h1>
-    <p>
-      This is the home page.
-    </p>
+    <h2 align="center" ><a href="https://github.com/baedal-project/baedal">프로젝트[Baedal]</a></h2>
+
+      <br>
+      <h4 align="center">📆 2022.11.04 ~ 2022.12.16</h4>
+
+        <br>
+        <br>
+        <h2 align="center">🛠 Tech Stack 🛠</h2>
+      <p align="center">
+        <img src="https://img.shields.io/badge/Spring-6DB33F?style=for-the-badge&logo=github&logoColor=white">
+        <img src="https://img.shields.io/badge/github-181717?style=for-the-badge&logo=github&logoColor=white">
+        <img src="https://img.shields.io/badge/linux-FCC624?style=for-the-badge&logo=linux&logoColor=black">
+        <img src="https://img.shields.io/badge/aws-232F3E?style=for-the-badge&logo=aws&logoColor=white">
+        <img src="https://img.shields.io/badge/Docker-2496ED?style=for-the-badge&logo=Docker&logoColor=white">
+      </p>
+
+
   </div>
 </template>
 
